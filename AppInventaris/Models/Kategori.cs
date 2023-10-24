6603e79dec677c316bdbf1bd9496ecdf232e6ed3
ocph23 <ocph23@gmail.com>
2023-10-25 07:05:19 +0900

    change category

--- AppInventaris/Models/Kategori.cs
+++ AppInventaris/Models/Kategori.cs
@@ -3,7 +3,7 @@ namespace   AppInventaris.Models;
 public  class Kategori
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public  string Name { get; set; }
     public string? Description { get; set; }
 
 }
